fdb95db698bdf88fe8c5ee695935858af10b13c7
MiltonRafael <miltonrafael166@gmail.com>
2026-01-06 13:03:02 -0300

    Start Project

---
+++ src/test/java/com/springpratics/gestao_custos/GestaoCustosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpratics.gestao_custos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCustosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
